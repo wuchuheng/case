0f7c2b530a7ead0c16700cda94ee881585b8b0a1
wuchuheng <wuchuheng@163.com>
2021-04-30 21:21:34 +0800

    feat:添加数据库和数据表初始化

--- src/backEndServer/index.js
+++ src/backEndServer/index.js
@@ -3,19 +3,23 @@ const {ApolloServer} = require('apollo-server')
 const CasesAPI = require('./datasources/CasesAPI')
 const typeDefs = require('./schema')
 const resolvers = require('./resolvers')
+const initDB = require('./boot/initDB')
+
+const DB = initDB()
 
 const server = new ApolloServer({
     typeDefs,
     resolvers,
     dataSources: () => ({
-        CasesAPI: new CasesAPI()
+        CasesAPI: new CasesAPI({DB})
     })
 })
 
-server.listen().then(() => {
+const port = 5000
+server.listen(port).then(() => {
     console.log(`
     Server is running!
-    Listening on port 4000
+    Listening on http://127.0.0.1:${port}
     Explore at https://studio.apollographql.com/dev
     `)
 })
